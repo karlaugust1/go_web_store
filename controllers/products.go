01dd7ece334bf379c57f5f2a0c55300c6a8ca782
Karl Harder <karl.harder@roit.com.br>
2021-06-21 20:35:07 -0300

    feat(insert): add methods to insert new product

--- controllers/products.go
+++ controllers/products.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"go_web_store/model"
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
@@ -11,4 +13,31 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	products := model.FindAll()
 	temp.ExecuteTemplate(w, "Index", products)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		name := r.FormValue("nome")
+		description := r.FormValue("descricao")
+		price := r.FormValue("preco")
+		quantity := r.FormValue("quantidade")
+
+		convertedPrice, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço")
+		}
+
+		convertedQuantity, err := strconv.Atoi(quantity)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade")
+		}
+
+		model.CreateNewProduct(name, description, convertedPrice, convertedQuantity)
+	}
+
+	http.Redirect(w, r, "/", 301)
 }
--- model/product.go
+++ model/product.go
@@ -42,3 +42,16 @@ func FindAll() []Product {
 
 	return products
 }
+
+func CreateNewProduct(name, description string, price float64, quantity int) {
+	db := db.ConectDatabase()
+
+	insertData, err := db.Prepare("insert into produtos(nome, descricao, preco, quantidade) values ($1,$2,$3,$4)")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	insertData.Exec(name, description, price, quantity)
+
+	defer db.Close()
+}
